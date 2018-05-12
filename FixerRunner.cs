6e10eb92c1306f8df5f07b17de1396469baf8be5
tolltech <alexandrovpe@gmail.com>
2018-05-12 17:49:10 +0400

    add overload for running without ninject

--- FixerRunner.cs
+++ FixerRunner.cs
@@ -42,5 +42,34 @@ namespace Tolltech.TollEnnobler
                 return false;
             }
         }
+
+        public bool Run(ISettings settings, IFixer[] fixers, ILog configuredLog = null)
+        {
+            try
+            {
+                var solutionProcessor = new SolutionProcessor(settings);
+
+                log = configuredLog ?? LogManager.GetLogger(typeof(FixerRunner));
+
+                var success = solutionProcessor.Process(settings.SolutionPath, fixers);
+
+                if (!success)
+                    log?.ToError($"Changes cant be applied!");
+                else
+                    log?.ToConsole($"Changes was applied!");
+
+                return success;
+            }
+            catch (Exception ex)
+            {
+                if (log == null)
+                    throw;
+
+                log.Error($"Something goes wrong", ex);
+                Console.WriteLine(ex.Message);
+
+                return false;
+            }
+        }
     }
 }
